320557e9850868d4b35b349ba1fb38b60bf4b520
Oscar <oscar.pino17@tectijuana.edu.mx>
2023-01-09 10:15:01 -0800

    final

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
         
     makeSound(buttonInnerHTML);
         
+    buttonAnimation(buttonInnerHTML);
     });
 }
 
@@ -59,4 +60,14 @@ function makeSound(key){
         default:
             break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
